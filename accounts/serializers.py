d8efc83ee3ecbc6f109849690925d0066b2252b6
badhranachu <www.badhranks678@gmail.com>
2026-02-22 23:57:51 +0530

    1157p

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -8,36 +8,18 @@ User = get_user_model()
 # ✅ Registration Serializer
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-    public_password = serializers.CharField(write_only=True, required=False)
 
     class Meta:
         model = User
-        fields = [
-            "username",
-            "email",
-            "password",
-            "is_public",
-            "public_password",
-        ]
+        fields = ["username", "email", "password"]
 
     def create(self, validated_data):
-        if validated_data.get("public_password"):
-            validated_data["public_password"] = make_password(
-                validated_data["public_password"]
-            )
-
         user = User.objects.create_user(
             username=validated_data["username"],
             email=validated_data.get("email"),
             password=validated_data["password"],
         )
-
-        user.is_public = validated_data.get("is_public", True)
-        user.public_password = validated_data.get("public_password")
-        user.save()
-
         return user
-
 
 # ✅ Profile Serializer
 class UserSerializer(serializers.ModelSerializer):
